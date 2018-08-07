2ff71d0c8574fb7961dbd2cb13f52ab221c323a4
LilySheYin <35239005+LilySheYin@users.noreply.github.com>
2018-08-07 16:59:46 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,13 +1,3 @@
-<html>
- <head>
-  <title>PHP w00t!</title>
- </head>
- <body>
- <center>
-   <?php
-   $timezone  = 1;
-   echo gmdate("j/m/Y H:i:s", time() + 3600*($timezone));
-   ?>
- </center>
- </body>
-</html>
+<?php
+phpinfo();
+?>
